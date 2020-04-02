3af56f00f00cfc89566c315eaf326ef9f568806a
yyyh <444510796@qq.com>
2020-04-02 17:32:22 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="ox">井 字 棋</div>
+    <div>第{{n}}步</div>
+    <div class="chess">
+      <div class="row">
+        <Cell @click="onClickCell(0, $event)" :n="n" />
+        <Cell @click="onClickCell(1, $event)" :n="n" />
+        <Cell @click="onClickCell(2, $event)" :n="n" />
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(3, $event)" :n="n" />
+        <Cell @click="onClickCell(4, $event)" :n="n" />
+        <Cell @click="onClickCell(5, $event)" :n="n" />
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(6, $event)" :n="n" />
+        <Cell @click="onClickCell(7, $event)" :n="n" />
+        <Cell @click="onClickCell(8, $event)" :n="n" />
+      </div>
+    </div>
+    <br>
+    <div>
+      {{result===null?'胜负未分':`胜方为${result}`}}
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script>
+  import Cell from "./Cell"
+  export default {
+    components: {
+      Cell
+    },
+    data() {
+      return {
+        n: 0,
+        map: [
+          [null, null, null],
+          [null, null, null],
+          [null, null, null]
+        ],
+        result: null
+      }
+    },
+    methods: {
+      onClickCell(i, text) {
+        console.log(`${i}号,内容是:${text}`)
+        this.map[Math.floor(i / 3)][i % 3] = text
+        if (this.n < 9) {
+          this.n += 1
+        } else {
+          return this.n
+        }
+        this.tell()
+      },
+      tell() {
+        const map = this.map
+        for (let i = 0; i < 2; i++) {
+          if (map[i][0] !== null &&
+            map[i][0] === map[i][1] &&
+            map[i][1] === map[i][2]
+          ) {
+            this.result = map[i][0]
+          }
+        }
+        for (let s = 0; s < 2; s++) {
+          if (map[0][s] !== null &&
+            map[0][s] === map[1][s] &&
+            map[1][s] === map[2][s]
+          ) {
+            this.result = map[0][s]
+          }
+        }
+        if (map[0][0] !== null &&
+          map[0][0] === map[1][1] &&
+          map[1][1] === map[2][2]
+        ) {
+          this.result = map[0][0]
+        }
+        if (map[0][2] !== null &&
+          map[0][2] === map[1][1] &&
+          map[1][1] === map[2][0]
+        ) {
+          this.result = map[0][2]
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .ox{
+    font-size: 30px;
+    font-weight: bold;
+  }
+  .row {
+    display: flex;
+  }
+  .wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
---
+++ src/Cell.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div class="cell" v-on:click="onClickSelf">
+            <template v-if="a">
+                <div v-if="X" class="X">
+                    <div class="x1"></div>
+                    <div class="x2"></div>
+                </div>
+                <div v-if="O" class="O"></div>
+            </template>
+            <template v-else></template>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["n"],
+        data() {
+            return {
+                a: false,
+                text:""
+            }
+        },
+        methods: {
+            onClickSelf() {
+                if(this.text!==""){
+                    return
+                }
+                this.a = true;
+                if (this.n % 2 === 0) {
+                    this.X = true
+                    this.O = false
+                    this.text = "X"
+                } else {
+                    this.X = false
+                    this.O = true
+                    this.text = "O"
+                }
+                this.$emit("click",this.text);
+            }
+        }
+    }
+</script>
+
+<style>
+    .O {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 5px solid rgb(255, 0, 0);
+    }
+
+    .x1 {
+        border: 5px solid black;
+        background-color: black;
+        width: 50px;
+        transform: rotate(45deg);
+        position: absolute;
+    }
+
+    .x2 {
+        border: 5px solid black;
+        background-color: black;
+        width: 50px;
+        transform: rotate(135deg);
+    }
+
+    .cell {
+        border: 3px solid black;
+        width: 80px;
+        height: 80px;
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+        margin: 0px 0px -3px -3px;
+    }
+</style>
